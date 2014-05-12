c0bd8c5c65f9180b1b330d6e08da68cca6c53fa5
Philip Demey <p.demey@opwoco.de>
2014-05-12 15:15:43 +0200

    Added method stubs

--- Service/ForecastService.php
+++ Service/ForecastService.php
@@ -3,7 +3,19 @@
 namespace opwoco\ForecastBundle\Service;
 
 class ForecastService {
+	private $forecast_url;
 
 	public function __construct($forecast_url) {
+		$this->forecast_url = $forecast_url;
+
+		// Get api key from config
+	}
+
+	public function getForecastByLocation($location) {
+
+	}
+	
+	public function getForecastByCoordinates($lat, $lng) {
+
 	}
 }
